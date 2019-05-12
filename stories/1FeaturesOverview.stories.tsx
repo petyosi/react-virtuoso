1992f7827106e903af04d8bbe1196dde271741e0
Petyo Ivanov <underlog@gmail.com>
2019-05-12 13:10:38 +0000

    Improve the descriptions

--- stories/1FeaturesOverview.stories.tsx
+++ stories/1FeaturesOverview.stories.tsx
@@ -3,6 +3,7 @@ import { Virtuoso } from '../src/Virtuoso'
 import { getUser } from './FakeData'
 import { ExampleListItem, ExampleAvatar, ExampleUserInfo } from './ExampleComponents'
 import { storiesOf } from '@storybook/react'
+import { ExampleInfo, ExampleTitle } from './ExampleInfo'
 
 const group = storiesOf('Features Overview', module)
 
@@ -26,9 +27,20 @@ const Item = (index: number) => {
 const HundredThousandItems = () => {
   return (
     <>
-      <h2>100 000 Items</h2>
+      <ExampleInfo>
+        <ExampleTitle>100 000 Items</ExampleTitle>
+        <p>
+          The Virtuoso component is built for the display of huge lists - you do not have to configure anything apart
+          from the total item count and the <code>item</code> prop renderer.
+        </p>
+
+        <p>
+          The prop render callback accepts <code>index</code> parameter, which specifies the absolute index of the item
+          being rendered; It is up to you to build and return the respective content for it.
+        </p>
+      </ExampleInfo>
       <div style={{ height: '500px', width: '300px' }}>
-        <Virtuoso overscan={0} totalCount={100000} item={Item} />
+        <Virtuoso totalCount={100000} item={Item} />
       </div>
     </>
   )
@@ -41,15 +53,14 @@ group.add('100 000 items', () => <HundredThousandItems />)
 const TopItems = () => {
   return (
     <>
-      <div className="example-description">
-        <h2>Fixed Top Items</h2>
+      <ExampleInfo>
+        <ExampleTitle>Fixed Top Items</ExampleTitle>
         <p>
-          {' '}
           The Virtuoso component accepts an optional <code>topItems</code> number property that allows you to pin the
-          first <code>N</code> items of the list.{' '}
+          first <code>N</code> items of the list.
         </p>
-        <p>Scroll the list below - the first two items remain fixed and always visible</p>
-      </div>
+        <p>Scroll the list below - the first two items remain fixed and always visible.</p>
+      </ExampleInfo>
 
       <div style={{ height: '500px', width: '300px' }}>
         <Virtuoso topItems={2} totalCount={100000} item={Item} />
@@ -65,14 +76,16 @@ group.add('Fixed top items', () => <TopItems />)
 const ListWithFooter = () => {
   return (
     <>
-      <div className="example-description">
-        <h2>Footer</h2>
+      <ExampleInfo>
+        <ExampleTitle>Footer</ExampleTitle>
         <p>
           The virtuoso component accepts an optional <code>footer</code> render property which is appended at the end of
-          the list. The footer can be used for loading indicators or "load more" buttons.
+          the list. The footer can be used for loading indicators or load more buttons.
         </p>
-        <p>Scroll to the bottom of the list to see "-- end reached --".</p>
-      </div>
+        <p>
+          Scroll to the bottom of the list to see <code>-- end reached --</code>.
+        </p>
+      </ExampleInfo>
 
       <div style={{ height: '500px', width: '300px' }}>
         <Virtuoso
@@ -92,14 +105,14 @@ group.add('Footer', () => <ListWithFooter />)
 const ResizableList = () => {
   return (
     <>
-      <div className="example-description">
-        <h2>Automatic Resizing</h2>
+      <ExampleInfo>
+        <ExampleTitle>Automatic Resizing</ExampleTitle>
         <p>
           The Virtuoso Component will automatically handle changes of the items&apos; heights (due to content being
           resized, images being loaded, etc).
         </p>
         <p>Resize your browser and scroll the list around - the items reposition correctly without overlap.</p>
-      </div>
+      </ExampleInfo>
 
       <div style={{ height: '500px', width: '50%' }}>
         <Virtuoso
---
+++ stories/ExampleInfo.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+// import React from 'react'
+
+export const ExampleInfo = styled.div`
+  font-family: Helvetica Neue, Helvetica, Segoe UI, Arial, freesans, sans-serif;
+  font-size: 14px;
+  line-height: 18px;
+  max-width: 630px;
+`
+export const ExampleTitle = styled.h2`
+  font-size: 20px;
+  line-height: 24px;
+`
