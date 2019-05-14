6dd82b11f5737826691f2c400c9fba976a4423e7
Petyo Ivanov <underlog@gmail.com>
2019-05-14 10:32:45 +0300

    Ugly title workaround

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -2,7 +2,6 @@ import { create } from '@storybook/theming'
 
 export default create({
   base: 'light',
-  title: 'Virtuoso',
   brandTitle: 'Virtuoso',
   brandUrl: 'https://virtuoso.dev',
   brandImage: 'https://user-images.githubusercontent.com/13347/57672903-af170d80-7622-11e9-865e-acf06a8428e6.png',
