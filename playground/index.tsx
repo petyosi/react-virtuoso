9976bdd82e3cf5c740c0acb951844f43526cf360
Petyo Ivanov <underlog@gmail.com>
2019-05-28 12:46:56 +0300

    playground

--- playground/index.tsx
+++ playground/index.tsx
@@ -14,8 +14,8 @@ const GroupContainer: TItemContainer = ({ children, ...props }) => {
   )
 }
 
-const ListContainer: TListContainer = ({ children, listRef, className }) => (
-  <ul className={className} ref={listRef}>
+const ListContainer: TListContainer = ({ children, listRef, style }) => (
+  <ul ref={listRef} style={style}>
     {children}
   </ul>
 )
@@ -32,52 +32,38 @@ const App = () => {
       }
       setGroupCounts(groups)
     }, 500)
-    /*
-    setTimeout(() => {
-      const groups = []
-      for (let index = 0; index < 20; index++) {
-        groups.push(10)
-      }
-      setGroupCounts(groups)
-    }, 1000)
-    setTimeout(() => {
-      const groups = []
-      for (let index = 0; index < 5; index++) {
-        groups.push(10)
-      }
-      setGroupCounts(groups)
-    }, 2000)
-     */
   }, [])
 
   return (
-    <div>
-      <button onClick={() => virtuoso.current.scrollToIndex(77)}>Go to 77</button>
-      <GroupedVirtuoso
-        ref={virtuoso}
-        style={{ height: '400px', width: '350px' }}
-        groupCounts={groupCounts}
-        ItemContainer={React.memo(ItemContainer)}
-        ListContainer={React.memo(ListContainer)}
-        GroupContainer={React.memo(GroupContainer)}
-        endReached={index => {
-          console.log('end reached', index)
-        }}
-        group={index => {
-          return (
-            <div style={{ height: '20px' }}>
-              Group {index * 10} &ndash; {index * 10 + 10}
-            </div>
-          )
-        }}
-        item={(index, groupIndex) => {
-          return (
-            <div style={{ height: '40px' }}>
-              {index} (group {groupIndex})
-            </div>
-          )
-        }}
-      />
+    <div style={{ display: 'block' }}>
+      <div>
+        <button onClick={() => virtuoso.current.scrollToIndex(77)}>Go to 77</button>
+        <GroupedVirtuoso
+          ref={virtuoso}
+          style={{ height: '400px', width: '350px' }}
+          groupCounts={groupCounts}
+          ItemContainer={React.memo(ItemContainer)}
+          ListContainer={React.memo(ListContainer)}
+          GroupContainer={React.memo(GroupContainer)}
+          endReached={index => {
+            console.log('end reached', index)
+          }}
+          group={index => {
+            return (
+              <div style={{ height: '20px' }}>
+                Group {index * 10} &ndash; {index * 10 + 10}
+              </div>
+            )
+          }}
+          item={(index, groupIndex) => {
+            return (
+              <div style={{ height: '40px' }}>
+                {index} (group {groupIndex})
+              </div>
+            )
+          }}
+        />
+      </div>
     </div>
   )
 }
