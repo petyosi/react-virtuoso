ca12ed4ebd31d4c07fd5d10749630a3b45c3d747
Petyo Ivanov <underlog@gmail.com>
2019-06-27 19:15:33 +0300

    Slight optimization

--- src/VirtuosoGridEngine.ts
+++ src/VirtuosoGridEngine.ts
@@ -1,4 +1,4 @@
-import { subject, map, scan, combineLatest, TObservable } from '../src/tinyrx'
+import { subject, combineLatest, withLatestFrom } from '../src/tinyrx'
 import { makeInput, makeOutput } from './rxio'
 
 type GridDimensions = [
@@ -22,32 +22,31 @@ export const VirtuosoGridEngine = () => {
   const totalCount$ = subject(0)
   const scrollTop$ = subject(0)
   const overscan$ = subject(0)
+  const itemRange$ = subject<GridItemRange>([0, 0])
+  const totalHeight$ = subject(0)
+  const listOffset$ = subject(0)
 
-  const itemRange$: TObservable<GridItemRange> = combineLatest(
-    gridDimensions$,
-    scrollTop$,
-    overscan$,
-    totalCount$
-  ).pipe(
-    scan(
-      (
-        itemRange,
-        [[containerWidth, containerHeight, itemWidth, itemHeight], scrollTop, overscan, totalCount]
-      ): GridItemRange => {
+  combineLatest(gridDimensions$, scrollTop$, overscan$, totalCount$)
+    .pipe(withLatestFrom(itemRange$))
+    .subscribe(
+      ([[[containerWidth, containerHeight, itemWidth, itemHeight], scrollTop, overscan, totalCount], itemRange]) => {
         if (itemWidth === undefined || itemHeight === undefined) {
-          return [0, 0] as GridItemRange
+          return
         }
 
         const [startIndex, endIndex] = itemRange
         const itemsPerRow = hackFloor(containerWidth / itemWidth)
 
-        const toRange = (down = true): GridItemRange => {
+        const updateRange = (down: boolean): void => {
           const [topOverscan, bottomOverscan] = down ? [0, overscan] : [overscan, 0]
 
-          const startIndex = itemsPerRow * floor((scrollTop - topOverscan) / itemHeight)
-          const endIndex = itemsPerRow * ceil((scrollTop + containerHeight + bottomOverscan) / itemHeight) - 1
+          let startIndex = itemsPerRow * floor((scrollTop - topOverscan) / itemHeight)
+          let endIndex = itemsPerRow * ceil((scrollTop + containerHeight + bottomOverscan) / itemHeight) - 1
 
-          return [max(0, startIndex), min(totalCount - 1, endIndex)]
+          startIndex = max(0, startIndex)
+          endIndex = min(totalCount - 1, endIndex)
+          itemRange$.next([startIndex, endIndex])
+          listOffset$.next(floor(startIndex / itemsPerRow) * itemHeight)
         }
 
         const listTop = itemHeight * floor(startIndex / itemsPerRow)
@@ -55,41 +54,15 @@ export const VirtuosoGridEngine = () => {
 
         // user is scrolling up - list top is below the top edge of the viewport
         if (listTop > scrollTop) {
-          return toRange(false)
+          updateRange(false)
+          // user is scrolling down - list bottom is above the bottom edge of the viewport
+        } else if (listBottom < scrollTop + containerHeight) {
+          updateRange(true)
         }
 
-        // user is scrolling down - list bottom is above the bottom edge of the viewport
-        if (listBottom < scrollTop + containerHeight) {
-          return toRange(true)
-        }
-
-        return itemRange
-      },
-      [0, 0]
+        totalHeight$.next(itemHeight * ceil(totalCount / itemsPerRow))
+      }
     )
-  )
-
-  const totalHeight$ = combineLatest(gridDimensions$, totalCount$).pipe(
-    map(([[containerWidth, _, itemWidth, itemHeight], totalCount]) => {
-      if (itemWidth === undefined || itemHeight === undefined) {
-        return 0
-      }
-      const itemsPerRow = hackFloor(containerWidth / itemWidth)
-      const rows = ceil(totalCount / itemsPerRow)
-      return itemHeight * rows
-    })
-  )
-
-  const listOffset$ = combineLatest(itemRange$, gridDimensions$).pipe(
-    map(([[startIndex], [containerWidth, _, itemWidth, itemHeight]]) => {
-      if (itemWidth === undefined || itemHeight === undefined) {
-        return 0
-      }
-
-      const itemsPerRow = hackFloor(containerWidth / itemWidth)
-      return floor(startIndex / itemsPerRow) * itemHeight
-    })
-  )
 
   return {
     gridDimensions: makeInput(gridDimensions$),
