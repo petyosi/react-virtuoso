c957b58fcc204f93df80dfb2735d9bf2f34e0ffc
Petyo Ivanov <underlog@gmail.com>
2019-07-01 19:44:45 +0300

    Debugging the grid

--- playground/grid.tsx
+++ playground/grid.tsx
@@ -1,29 +1,53 @@
-import React, { FC, useRef } from 'react'
+import React, { useRef, useEffect, useCallback, useState } from 'react'
 import * as ReactDOM from 'react-dom'
 
 import { VirtuosoGrid } from '../src/VirtuosoGrid'
+import { getUser } from '../../site/src/examples/FakeData'
+import { UserItem } from '../../site/src/examples/ExampleComponents'
+import './grid.css'
 
-const Item: FC<{ index: number }> = React.memo(({ index }) => {
-  console.log('rendering', index)
-  return <span>Item {index}</span>
-})
-
-Item.displayName = 'VirtuosoItem'
+const GenerateItem = (index: number) => {
+  return <UserItem user={getUser(index)} index={index} />
+}
 
 const App = () => {
-  const ref = useRef<VirtuosoGrid>(null)
-  return (
-    <div>
-      <button onClick={() => ref.current.scrollToIndex(300)}>Scroll to 300</button>
+  const [total, setTotal] = useState(0)
+  const items = useRef([])
+  const loading = useRef(false)
 
-      <VirtuosoGrid
-        ref={ref}
-        totalCount={100}
-        scrollingStateChange={scrolling => console.log({ scrolling })}
-        endReached={end => console.log({ end })}
-        item={index => <Item index={index} />}
-      />
-    </div>
+  // the setTimeout below simulates a network request.
+  // In the real world, you can fetch data from a service.
+  // the setTotal call will trigger a refresh for the list,
+  // so make sure you call it last
+  const loadMore = useCallback(() => {
+    if (loading.current || total >= 300) {
+      return
+    }
+    console.log(total)
+
+    loading.current = true
+
+    setTimeout(() => {
+      for (let index = total; index < total + total + 100; index++) {
+        items.current = [...items.current, getUser(index)]
+      }
+      loading.current = false
+      setTotal(items.current.length)
+    }, 500)
+  }, [total])
+
+  useEffect(() => {
+    loadMore()
+  }, [])
+
+  return (
+    <VirtuosoGrid
+      style={{ width: '100%', height: '500px' }}
+      overscan={200}
+      totalCount={total}
+      item={GenerateItem}
+      endReached={() => loadMore()}
+    />
   )
 }
 
