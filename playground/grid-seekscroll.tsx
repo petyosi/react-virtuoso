901195ea58b215ec6e8d6e0953bb869b1ce98d14
kiyoshimori <darkf0x13566@gmail.com>
2020-08-14 21:08:41 +0300

    clear playground tsx from useless parts

--- playground/grid-seekscroll.tsx
+++ playground/grid-seekscroll.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import { useRef, useEffect, useCallback, useState } from 'react'
 import * as ReactDOM from 'react-dom'
 
 import { VirtuosoGrid } from '../src/VirtuosoGrid'
@@ -15,58 +14,20 @@ const GenerateItem = (index: number) => {
 }
 
 const App = () => {
-  const [total, setTotal] = useState(0)
-  const items = useRef([])
-  const loading = useRef(false)
-
-  // the setTimeout below simulates a network request.
-  // In the real world, you can fetch data from a service.
-  // the setTotal call will trigger a refresh for the list,
-  // so make sure you call it last
-  const loadMore = useCallback(() => {
-    if (loading.current || total >= 300) {
-      return
-    }
-
-    loading.current = true
-
-    setTimeout(() => {
-      for (let index = total; index < total + total + 100; index++) {
-        items.current = [...items.current, getUser(index)]
-      }
-      loading.current = false
-      setTotal(items.current.length)
-    }, 500)
-  }, [total])
-
-  useEffect(() => {
-    loadMore()
-  }, [])
+  const items = Array.from({ length: 300 }).map((_, index) => getUser(index))
 
   return (
     <div>
-      <button onClick={() => setTotal(0)}>Set to Zero</button>
-      <button onClick={() => setTotal(2)}>Set to 2</button>
-      <button onClick={() => setTotal(1000)}>Restore</button>
       <VirtuosoGrid
         style={{ width: '100%', height: '500px' }}
         overscan={200}
-        totalCount={total}
+        totalCount={items.length}
         item={GenerateItem}
-        endReached={() => loadMore()}
         itemClassName="virtuoso-grid-placeholder-item"
         scrollSeek={{
           enter: velocity => Math.abs(velocity) > 200,
-          exit: velocity => {
-            const shouldExit = Math.abs(velocity) < 30
-            if (shouldExit) {
-              // setVisibleRange(['-', '-'])
-            }
-            return shouldExit
-          },
+          exit: velocity => Math.abs(velocity) < 30,
           change: (_velocity, { startIndex, endIndex }) => console.log(startIndex, endIndex),
-          // You can use index to randomize
-          // and make the placeholder list more organic.
           placeholder: ({ index, height }) => (
             <div style={{ height }} className="placeholder">
               {index}
