457c6fff6b718512c40795f50ad1404c44bee586
Petyo Ivanov <underlog@gmail.com>
2020-11-29 17:49:18 +0200

    doc: fix site build

--- site/docs/grid-responsive-columns.md
+++ site/docs/grid-responsive-columns.md
@@ -7,13 +7,13 @@ slug: /grid-responsive-columns/
 
 The `VirtuosoGrid` component displays **equally sized items**, while supporting multiple items per row.
 
-The component provides no styling out of the box; the styling and the layout of the items can be specified by passing two properties - `itemClassName` and `listClassName`. 
-Alternatively, you can use styled components and pass them as `components.Item` and `components.List`. 
+The component provides no styling out of the box; the styling and the layout of the items can be specified by passing two properties - `itemClassName` and `listClassName`.
+Alternatively, you can use styled components and pass them as `components.Item` and `components.List`.
 Either way, it is up to you to implement the layout with Flexbox or CSS grid. You can use plain old CSS or CSS in JS - the example uses Emotion.
 
 ### Responsive Layout
 
-You can safely use media queries, `min-width`, percentages etc. in the item layout definitions. 
+You can safely use media queries, `min-width`, percentages etc. in the item layout definitions.
 The grid observes the container / item dimensions and recalculates the scroll size accordingly.
 
 Resize your browser and scroll around - the items reposition correctly.
@@ -34,7 +34,7 @@ Resize your browser and scroll around - the items reposition correctly.
     @media (max-width: 480px) {
       width: 100%;
     }
-  `;
+  `
 
   const ItemWrapper = styled.div`
     flex: 1;
@@ -43,29 +43,31 @@ Resize your browser and scroll around - the items reposition correctly.
     padding: 1rem 1rem;
     border: 1px solid var(--ifm-hr-border-color);
     white-space: nowrap;
-  `;
+  `
 
   const ListContainer = styled.div`
     display: flex;
     flex-wrap: wrap;
-  `;
+  `
 
   return (
     <VirtuosoGrid
       totalCount={10000}
       overscan={200}
       components={{
-            Item: ItemContainer,
-            List: ListContainer
-      }}
-      item={(index) => <ItemWrapper>Item {index}</ItemWrapper>}
-      scrollSeek={{
-        enter: (velocity) => Math.abs(velocity) > 200,
-        exit: (velocity) => Math.abs(velocity) < 30,
-        change: (_, range) => console.log({ range }),
-        placeholder: ({ height, index }) => (
-          <ItemContainer><ItemWrapper>{"--"}</ItemWrapper></ItemContainer>
+        Item: ItemContainer,
+        List: ListContainer,
+        ScrollSeekPlaceholder: ({ height, index }) => (
+          <ItemContainer>
+            <ItemWrapper>{'--'}</ItemWrapper>
+          </ItemContainer>
         ),
+      }}
+      itemContent={index => <ItemWrapper>Item {index}</ItemWrapper>}
+      scrollSeekConfiguration={{
+        enter: velocity => Math.abs(velocity) > 200,
+        exit: velocity => Math.abs(velocity) < 30,
+        change: (_, range) => console.log({ range }),
       }}
     />
   )
--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -1,108 +1,56 @@
-const path = require("path");
+const path = require('path')
 
 module.exports = {
-  title: "React Virtuoso",
-  tagline: "Powerful, Elegant and Simple Virtual List for React",
-  url: "https://virtuoso.dev/",
-  baseUrl: "/",
-  onBrokenLinks: "throw",
-  favicon: "img/favicon.ico",
-  organizationName: "virtuoso.dev", // Usually your GitHub org/user name.
-  projectName: "react-virtuoso", // Usually your repo name.
+  title: 'React Virtuoso',
+  tagline: 'Powerful, Elegant and Simple Virtual List for React',
+  url: 'https://virtuoso.dev/',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  favicon: 'img/favicon.ico',
+  organizationName: 'virtuoso.dev', // Usually your GitHub org/user name.
+  projectName: 'react-virtuoso', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: "",
+      title: '',
       logo: {
-        alt: "React Virtuoso",
-        src: "img/logo.svg",
+        alt: 'React Virtuoso',
+        src: 'img/logo.svg',
       },
       items: [
+        { to: 'blog', label: 'Blog', position: 'left' },
         {
-          to: "docs/",
-          activeBasePath: "docs",
-          label: "Docs",
-          position: "left",
-        },
-        { to: "blog", label: "Blog", position: "left" },
-        {
-          href: "https://github.com/petyosi/react-virtuoso",
-          label: "GitHub",
-          position: "right",
+          href: 'https://github.com/petyosi/react-virtuoso',
+          label: 'GitHub',
+          position: 'right',
         },
       ],
     },
     footer: {
-      style: "dark",
-      links: [
-        {
-          title: "Docs",
-          items: [
-            {
-              label: "Style Guide",
-              to: "docs/",
-            },
-            {
-              label: "Second Doc",
-              to: "docs/doc2/",
-            },
-          ],
-        },
-        {
-          title: "Community",
-          items: [
-            {
-              label: "Stack Overflow",
-              href: "https://stackoverflow.com/questions/tagged/docusaurus",
-            },
-            {
-              label: "Discord",
-              href: "https://discordapp.com/invite/docusaurus",
-            },
-            {
-              label: "Twitter",
-              href: "https://twitter.com/docusaurus",
-            },
-          ],
-        },
-        {
-          title: "More",
-          items: [
-            {
-              label: "Blog",
-              to: "blog",
-            },
-            {
-              label: "GitHub",
-              href: "https://github.com/facebook/docusaurus",
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      style: 'dark',
+      links: [],
+      copyright: `Copyright © ${new Date().getFullYear()} Petyo Ivanov. Built with Docusaurus.`,
     },
   },
-  themes: ["@docusaurus/theme-live-codeblock"],
+  themes: ['./docusaurus-theme-live-codeblock'],
   presets: [
     [
-      "@docusaurus/preset-classic",
+      '@docusaurus/preset-classic',
       {
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-          routeBasePath: "/",
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/',
           // Please change this to your repo.
-          editUrl:
-            "https://github.com/facebook/docusaurus/edit/master/website/",
+          editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl:
-            "https://github.com/facebook/docusaurus/edit/master/website/blog/",
+          editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/blog/',
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
       },
     ],
   ],
-};
+}
