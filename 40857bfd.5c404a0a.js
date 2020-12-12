6cbe06340ecae317c42cb656d0f6df068efdc9ae
Petyo Ivanov <underlog@gmail.com>
2020-12-12 10:21:41 +0000

    Deploy website - based on da85b2b84e60771e6253f430f953db98da0153c7

---
+++ 40857bfd.5c404a0a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{83:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/blog/welcome"},{"title":"Hello","permalink":"/blog/hello-world"},{"title":"Hola","permalink":"/blog/hola"}]}')}}]);
