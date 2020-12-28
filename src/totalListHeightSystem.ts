815ec0c8b9b6a760427058df324eb1de1a1eae60
Petyo Ivanov <underlog@gmail.com>
2020-12-28 11:19:05 +0200

    refactor: use star import

--- src/totalListHeightSystem.ts
+++ src/totalListHeightSystem.ts
@@ -1,24 +1,24 @@
-import { pipe, system, tup, combineLatest, map, stream, connect, statefulStreamFromEmitter, duc } from '@virtuoso.dev/urx'
+import * as u from '@virtuoso.dev/urx'
 import { listStateSystem } from './listStateSystem'
 import { sizeRangeSystem } from './sizeRangeSystem'
 
-export const totalListHeightSystem = system(
+export const totalListHeightSystem = u.system(
   ([{ footerHeight, headerHeight }, { listState }]) => {
-    const totalListHeightChanged = stream<number>()
-    const totalListHeight = statefulStreamFromEmitter(
-      pipe(
-        combineLatest(footerHeight, headerHeight, listState),
-        map(([footerHeight, headerHeight, listState]) => {
+    const totalListHeightChanged = u.stream<number>()
+    const totalListHeight = u.statefulStreamFromEmitter(
+      u.pipe(
+        u.combineLatest(footerHeight, headerHeight, listState),
+        u.map(([footerHeight, headerHeight, listState]) => {
           return footerHeight + headerHeight + listState.offsetBottom + listState.bottom
         })
       ),
       0
     )
 
-    connect(duc(totalListHeight), totalListHeightChanged)
+    u.connect(u.duc(totalListHeight), totalListHeightChanged)
 
     return { totalListHeight, totalListHeightChanged }
   },
-  tup(sizeRangeSystem, listStateSystem),
+  u.tup(sizeRangeSystem, listStateSystem),
   { singleton: true }
 )
