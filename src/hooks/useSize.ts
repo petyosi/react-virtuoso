5475a10d873811b056d059c85fa419e764b70181
Petyo Ivanov <underlog@gmail.com>
2021-01-24 10:17:11 +0200

    fix(): revert RAF for useSize
    Fixes #269
    Opens #254
    
    A workaround is present here:
    https://github.com/DevExpress/testcafe/issues/4857#issuecomment-598775956

--- src/hooks/useSize.ts
+++ src/hooks/useSize.ts
@@ -7,16 +7,19 @@ export function useSizeWithElRef(callback: (e: HTMLElement) => void, enabled: bo
   const ref = useRef<CallbackRefParam>(null)
   const observer = new ResizeObserver(entries => {
     const element = entries[0].target as HTMLElement
+    // Revert the RAF below - it causes a blink in the upward scrolling fix
+    // See e2e/chat example
     // Avoid Resize loop limit exceeded error
     // https://github.com/edunad/react-virtuoso/commit/581d4558f2994adea375291b76fe59605556c08f
-    requestAnimationFrame(() => {
-      // if display: none, the element won't have an offsetParent
-      // measuring it at this mode is not going to work
-      // https://stackoverflow.com/a/21696585/1009797
-      if (element.offsetParent !== null) {
-        callback(element)
-      }
-    })
+    // requestAnimationFrame(() => {
+    //
+    // if display: none, the element won't have an offsetParent
+    // measuring it at this mode is not going to work
+    // https://stackoverflow.com/a/21696585/1009797
+    if (element.offsetParent !== null) {
+      callback(element)
+    }
+    // })
   })
 
   const callbackRef = (elRef: CallbackRefParam) => {
