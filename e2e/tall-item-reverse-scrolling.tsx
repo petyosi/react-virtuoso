254898a36142b5765d2dce08804c0ebbeed5d53d
Petyo Ivanov <underlog@gmail.com>
2021-01-28 17:21:10 +0200

    chore(): failing test case for very tall items

---
+++ e2e/tall-item-reverse-scrolling.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import { Virtuoso } from '../src/'
+
+export default function App() {
+  return (
+    <Virtuoso
+      totalCount={100}
+      itemContent={index => (
+        <div style={{ border: '1px solid black' }}>
+          Item {index}
+          <div>
+            {Array.from({ length: index % 2 ? 3 : 20 }, (_, i) => (
+              <div key={i}>Line {i}</div>
+            ))}
+          </div>
+        </div>
+      )}
+      initialTopMostItemIndex={99}
+      style={{ height: 300 }}
+    />
+  )
+}
