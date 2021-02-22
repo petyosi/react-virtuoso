ad23618add8e2412ee74cdef8848c62ae491e9bc
Petyo Ivanov <underlog@gmail.com>
2021-02-22 15:31:31 +0200

    chore(): fix erratic test

--- e2e/initial-scroll-top.test.ts
+++ e2e/initial-scroll-top.test.ts
@@ -1,14 +1,14 @@
-describe('scroll to index', () => {
+describe('initial scroll top', () => {
   beforeAll(async () => {
     await page.goto('http://localhost:1234/initial-scroll-top')
   })
 
   beforeEach(async () => {
     await page.reload()
-    await page.waitForTimeout(200) // :(
+    await page.waitForTimeout(500) // :(
   })
 
-  it('scrolls to 50px', async () => {
+  it('starts from 50px', async () => {
     const scrollTop = await page.evaluate(() => {
       const listContainer = document.querySelectorAll('#test-root > div')[0]
       return listContainer.scrollTop
