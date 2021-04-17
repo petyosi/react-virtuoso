fc0780dedcd83aa39bc8365c6f3ff00b176b25ed
Petyo Ivanov <underlog@gmail.com>
2021-04-17 16:51:20 +0300

    chore(): endless scroll playground

---
+++ e2e/endless-scrolling.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react'
+import { useState, useEffect, useCallback } from 'react'
+import { Virtuoso } from '../src'
+
+function generateItems(length: number, startIndex: number) {
+  return Array.from({ length }, (_, index) => `My Item ${index + startIndex}, gen: ${startIndex}`)
+}
+
+const itemContent = (_: number, data: string) => {
+  return <div style={{ height: 30 }}>{data}</div>
+}
+
+export default function App() {
+  const [items, setItems] = useState<string[]>(() => [])
+
+  const loadMore = useCallback(() => {
+    console.log('loading...')
+    return setTimeout(() => {
+      console.log('loaded')
+
+      setItems((items) => [...items, ...generateItems(100, items.length)])
+    }, 0)
+  }, [setItems])
+
+  useEffect(() => {
+    const timeout = loadMore()
+    return () => clearTimeout(timeout)
+  }, [loadMore])
+
+  return (
+    <div>
+      {items.length ? (
+        <Virtuoso data={items} initialItemCount={items.length} endReached={loadMore} itemContent={itemContent} style={{ height: 300 }} />
+      ) : (
+        <div>Loading</div>
+      )}
+    </div>
+  )
+}
