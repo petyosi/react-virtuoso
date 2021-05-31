d3bf34d655d6d1d45e6a4a239f31f3e8d06ff241
Stephen Haberman <stephen.haberman@gmail.com>
2021-05-31 14:10:20 -0500

    Remove original attempt.

--- src/hooks/useChangedChildSizes.ts
+++ src/hooks/useChangedChildSizes.ts
@@ -29,11 +29,6 @@ function getChangedChildSizes(children: HTMLCollection, itemSize: SizeFunction, 
     const index = parseInt(child.dataset.index!)
     const knownSize = parseInt(child.dataset.knownSize!)
     const size = itemSize(child, field)
-    // If size is zero, we might be in a CSS Grid that's using `display: contents`
-    // on the Item and/or itemContent or both, so look up to 2 levels down for the 1st cell.
-    // for (let i = 0, current = child.firstElementChild; i < 2 && size === 0 && current; i++, current = current.firstElementChild) {
-    //   size = (current as HTMLElement)[field]
-    // }
 
     if (size === 0) {
       throw new Error('Zero-sized element, this should not happen')
