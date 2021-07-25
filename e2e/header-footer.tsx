3d4feee90cb892ca0acf035a06a8fd86c33bcaae
cryptoethic <cryptoethic@gmail.com>
2021-07-25 22:56:48 +0200

    fix: lint

--- e2e/header-footer.tsx
+++ e2e/header-footer.tsx
@@ -4,21 +4,14 @@ import { Virtuoso } from '../src'
 export default function App() {
   return (
     <Virtuoso
-        totalCount={100}
-        components={{
-          Header: () => <div style={{ height: 150 }}>Header</div>,
-          Footer: () => <div style={{ height: 60 }}>Footer</div>
-        }}
-        itemContent={(index) => ( 
-            <div
-              style={{ height: 150, padding: "100px 0px", background: "gray" }}
-            >
-              Item {index}
-            </div> 
-        )}
-        style={{ height: 600 }}
-        
-        overscan={150}
-      />
+      totalCount={100}
+      components={{
+        Header: () => <div style={{ height: 150 }}>Header</div>,
+        Footer: () => <div style={{ height: 60 }}>Footer</div>,
+      }}
+      itemContent={(index) => <div style={{ height: 150, padding: '100px 0px', background: 'gray' }}>Item {index}</div>}
+      style={{ height: 600 }}
+      overscan={150}
+    />
   )
 }
