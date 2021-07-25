bd1189b33e6593225b9a84e2f4230d96a3f82f79
cryptoethic <cryptoethic@gmail.com>
2021-07-25 22:41:30 +0200

    fix: size range system for list with header

--- e2e/header-footer.tsx
+++ e2e/header-footer.tsx
@@ -4,14 +4,21 @@ import { Virtuoso } from '../src'
 export default function App() {
   return (
     <Virtuoso
-      totalCount={100}
-      topItemCount={2}
-      components={{
-        Header: () => <div style={{ height: 150 }}>Header</div>,
-        Footer: () => <div style={{ height: 60 }}>Footer</div>,
-      }}
-      itemContent={(index) => <div style={{ height: 150, background: 'gray' }}>Item {index}</div>}
-      style={{ height: 600 }}
-    />
+        totalCount={100}
+        components={{
+          Header: () => <div style={{ height: 150 }}>Header</div>,
+          Footer: () => <div style={{ height: 60 }}>Footer</div>
+        }}
+        itemContent={(index) => ( 
+            <div
+              style={{ height: 150, padding: "100px 0px", background: "gray" }}
+            >
+              Item {index}
+            </div> 
+        )}
+        style={{ height: 600 }}
+        
+        overscan={150}
+      />
   )
 }
