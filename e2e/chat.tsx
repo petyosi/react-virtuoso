a20b3b420f0d8f0893dc5760010f0ffe9352c516
Petyo Ivanov <underlog@gmail.com>
2021-09-03 20:17:17 +0300

    chore(): fix example

--- e2e/chat.tsx
+++ e2e/chat.tsx
@@ -1,4 +1,5 @@
-import React, { useRef, useState } from 'react'
+import { useRef, useState } from 'react'
+import * as React from 'react'
 import styled from '@emotion/styled'
 import { Virtuoso } from '../src/'
 import faker from 'faker'
