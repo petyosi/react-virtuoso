367b8ee07f2950c52bc32154199d5a9b55648630
Gustavo de Paula <gustavodepaula@disroot.org>
2021-11-22 10:32:00 -0300

    fix: globalThis fallback (#503)

--- src/loggerSystem.ts
+++ src/loggerSystem.ts
@@ -5,6 +5,11 @@ declare namespace globalThis {
   let VIRTUOSO_LOG_LEVEL: LogLevel | undefined
 }
 
+// eslint-disable-next-line @typescript-eslint/no-namespace
+declare namespace window {
+  let VIRTUOSO_LOG_LEVEL: LogLevel | undefined
+}
+
 export enum LogLevel {
   DEBUG,
   INFO,
@@ -26,11 +31,13 @@ const CONSOLE_METHOD_MAP = {
   [LogLevel.ERROR]: 'error',
 } as const
 
+const getGlobalThis = () => (typeof globalThis === 'undefined' ? window : globalThis)
+
 export const loggerSystem = u.system(
   () => {
     const logLevel = u.statefulStream<LogLevel>(LogLevel.ERROR)
     const log = u.statefulStream<Log>((label: string, message: any, level: LogLevel = LogLevel.INFO) => {
-      const currentLevel = (globalThis || window)['VIRTUOSO_LOG_LEVEL'] ?? u.getValue(logLevel)
+      const currentLevel = getGlobalThis()['VIRTUOSO_LOG_LEVEL'] ?? u.getValue(logLevel)
       if (level >= currentLevel) {
         console[CONSOLE_METHOD_MAP[level]]('%creact-virtuoso: %c%s %o', 'color: #0253b3; font-weight: bold', 'color: black', label, message)
       }
