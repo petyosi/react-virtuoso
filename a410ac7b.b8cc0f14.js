11027b524e45e9d7356f22607c80f779718a16fd
Petyo Ivanov <underlog@gmail.com>
2022-04-13 13:04:58 +0000

    Deploy website - based on cb75c23e03ad90f7526707a4e297387e080ce7d6

--- a410ac7b.b8cc0f14.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[57],{118:function(a){a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"urx","name":"urx","count":1,"permalink":"/blog/tags/urx"}')}}]);
