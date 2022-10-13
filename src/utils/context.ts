55b0bea8bd2e5bb6fe91270938215b911ace7cf2
Mihkel Eidast <mihkel@sixfold.com>
2022-10-13 19:17:05 +0300

    feat: mock context for grid

--- src/utils/context.ts
+++ src/utils/context.ts
@@ -6,3 +6,12 @@ export interface VirtuosoMockContextValue {
 }
 
 export const VirtuosoMockContext = React.createContext<VirtuosoMockContextValue | undefined>(undefined)
+
+export interface VirtuosoGridMockContextValue {
+  viewportHeight: number
+  viewportWidth: number
+  itemHeight: number
+  itemWidth: number
+}
+
+export const VirtuosoGridMockContext = React.createContext<VirtuosoGridMockContextValue | undefined>(undefined)
