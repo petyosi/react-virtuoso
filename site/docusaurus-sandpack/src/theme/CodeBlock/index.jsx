169c2b261be8b892fba0293996275ad7dfa8ad90
Petyo Ivanov <underlog@gmail.com>
2022-11-16 15:30:33 +0200

    chore: reverse faker-js from online samples

--- site/docusaurus-sandpack/src/theme/CodeBlock/index.jsx
+++ site/docusaurus-sandpack/src/theme/CodeBlock/index.jsx
@@ -4,7 +4,7 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import './style.css'
 
 const dataCode = `
-import { faker } from '@faker-js/faker'
+import faker from 'faker'
 import { groupBy } from 'lodash'
 import React from 'react'
 
