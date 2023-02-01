26d0dc6a0f9582d87b88596212bb776d6d233e73
Mihkel Eidast <mihkel@sixfold.com>
2023-02-01 18:33:50 +0200

    fix: skip recalc if data changes before totalCount

--- test/Virtuoso.test.tsx
+++ test/Virtuoso.test.tsx
@@ -176,4 +176,93 @@ describe('Virtuoso', () => {
 
     expect(listParent.firstElementChild.textContent).toBe('Item 1')
   })
+
+  it('updates when data is propagated (reduced length)', () => {
+    const itemsRendered = vi.fn()
+    const Case = () => {
+      const [data, setData] = React.useState([{ name: 'Item 0' }, { name: 'Item 1' }] as any[])
+
+      return (
+        <>
+          <Virtuoso
+            data={data}
+            itemContent={(_: number, item: { name: string }) => {
+              return item.name
+            }}
+            itemsRendered={itemsRendered}
+          />
+          <button onClick={() => setData([{ name: 'Item 1' }])}>Set Data</button>
+        </>
+      )
+    }
+
+    act(() => {
+      ReactDOM.createRoot(container).render(<Case />)
+    })
+
+    itemsRendered.mockClear()
+
+    const scroller = container.firstElementChild as any
+    const viewport = scroller.firstElementChild
+    const listParent = viewport.firstElementChild
+
+    act(() => {
+      scroller.triggerScroll({ scrollTop: 0, scrollHeight: 700, viewportHeight: 200 })
+      viewport.triggerResize({ getBoundingClientRect: () => ({ height: 100 }) })
+      listParent.triggerChangedChildSizes([{ startIndex: 0, endIndex: 0, size: 10 }])
+    })
+
+    expect(itemsRendered).toHaveBeenCalledTimes(2)
+    expect(itemsRendered).toHaveBeenNthCalledWith(1, [
+      {
+        data: {
+          name: 'Item 0',
+        },
+        index: 0,
+        offset: 0,
+        originalIndex: 0,
+        size: 0,
+      },
+    ])
+    expect(itemsRendered).toHaveBeenNthCalledWith(2, [
+      {
+        data: {
+          name: 'Item 0',
+        },
+        index: 0,
+        offset: 0,
+        originalIndex: 0,
+        size: 10,
+      },
+      {
+        data: {
+          name: 'Item 1',
+        },
+        index: 1,
+        offset: 10,
+        originalIndex: 1,
+        size: 10,
+      },
+    ])
+    itemsRendered.mockClear()
+
+    act(() => {
+      container.querySelector('button')!.dispatchEvent(new MouseEvent('click', { bubbles: true }))
+    })
+
+    expect(itemsRendered).toHaveBeenCalledTimes(1)
+    expect(itemsRendered).toHaveBeenCalledWith([
+      {
+        data: {
+          name: 'Item 1',
+        },
+        index: 0,
+        offset: 0,
+        originalIndex: 0,
+        size: 10,
+      },
+    ])
+
+    expect(listParent.firstElementChild.textContent).toBe('Item 1')
+  })
 })
