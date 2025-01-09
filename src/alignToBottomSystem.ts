76193dd71e1c4814909773ee399ab87b5c385fdf
Godwill Barasa <33608094+godwillcodes@users.noreply.github.com>
2025-01-09 12:35:54 +0300

    Update alignToBottomSystem.ts

--- src/alignToBottomSystem.ts
+++ src/alignToBottomSystem.ts
@@ -6,14 +6,13 @@ export const alignToBottomSystem = u.system(
   ([{ viewportHeight }, { totalListHeight }]) => {
     const alignToBottom = u.statefulStream(false)
 
-    // keep this for the table component only
     const paddingTopAddition = u.statefulStreamFromEmitter(
       u.pipe(
-        u.combineLatest(alignToBottom, viewportHeight, totalListHeight),
+        u.combineLatest([alignToBottom, viewportHeight, totalListHeight]),
         u.filter(([enabled]) => enabled),
-        u.map(([, viewportHeight, totalListHeight]) => {
-          return Math.max(0, viewportHeight - totalListHeight)
-        }),
+        u.map(([, viewportHeight, totalListHeight]) => 
+          Math.max(0, viewportHeight - totalListHeight)
+        ),
         u.throttleTime(0),
         u.distinctUntilChanged()
       ),
