6cc7aa3f8baeaa9e3ee71b1a1660fea7472000c1
Petyo Ivanov <underlog@gmail.com>
2025-03-29 06:52:07 +0200

    Fix build

--- packages/react-virtuoso/playwright.config.ts
+++ packages/react-virtuoso/playwright.config.ts
@@ -3,7 +3,7 @@ import { PlaywrightTestConfig } from '@playwright/test'
 const config: PlaywrightTestConfig = {
   testDir: './e2e',
   webServer: {
-    command: 'npm run dev',
+    command: 'npm run ladle',
     port: 61000,
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
